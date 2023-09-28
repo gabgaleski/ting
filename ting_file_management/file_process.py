f97889a0734e95bbd7828919387190d631abb257
Gabriel Galeski <galeski35@gmail.com>
2023-09-28 15:50:15 -0300

    feat: implementa função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance[i]["nome_do_arquivo"] == path_file:
+            return None
+
+    files = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(files),
+        "linhas_do_arquivo": (files),
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -19,4 +19,5 @@ class Queue(AbstractQueue):
     def search(self, index):
         if index < 0 or index >= len(self._data):
             raise IndexError('Índice Inválido ou Inexistente')
+
         return self._data[index]
