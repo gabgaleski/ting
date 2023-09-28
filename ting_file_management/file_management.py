e722d54fa6edd907715466097d32af8a9ebee6ba
Gabriel Galeski <galeski35@gmail.com>
2023-09-28 14:29:21 -0300

    feat: implementa file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+    elif not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return []
+    else:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
