503c38849cc45636e33812abb390ddd73ad2ad48
Gabriel Galeski <galeski35@gmail.com>
2023-09-28 18:15:01 -0300

    feat: implementa exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        file = instance.search(i)
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line, line_num in enumerate(file["linhas_do_arquivo"], 1):
+            if word.lower() in line_num.lower():
+                data["ocorrencias"].append(
+                    {
+                        "linha": line,
+                    }
+                )
+
+        if len(data["ocorrencias"]) > 0:
+            result.append(data)
+
+    return result
 
 
 def search_by_word(word, instance):
